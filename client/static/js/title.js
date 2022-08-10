d1f2667646e342582a7c865048a7fd25b2d0f8e8
vivian <vivian.v18723@gmail.com>
2022-08-10 15:11:56 +0100

    add key down function

--- client/static/js/title.js
+++ client/static/js/title.js
@@ -1,9 +1,17 @@
+// label
+const titleLabel = document.getElementById("titleLabel");
 // title input
 const title = document.getElementById("title");
 
 title.addEventListener("click", () => {
+  // icons
   iconsBg.style.display = "none";
   left.style.display = "none";
   right.style.display = "none";
   camera.style.display = "none";
+});
+
+title.addEventListener("keydown", () => {
+  // label
+  titleLabel.style.display = "inline";
 });
--- client/static/js/yourName.js
+++ client/static/js/yourName.js
@@ -1,9 +1,18 @@
+// label
+const authorLabel = document.getElementById("authorLabel");
 // input field
 const yourName = document.getElementById("yourName");
 
 yourName.addEventListener("click", () => {
+  // icons
   iconsBg.style.display = "none";
   left.style.display = "none";
   right.style.display = "none";
   camera.style.display = "none";
+});
+
+yourName.addEventListener("keydown", () => {
+  // label
+  authorLabel.style.display = "inline";
+  authorLabel.style.transition = "all 42s";
 });
