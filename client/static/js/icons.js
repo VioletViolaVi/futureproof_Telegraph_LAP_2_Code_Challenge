487aaf4d5bf7c05d63b27a5674e94cab22e7a872
vivian <vivian.v18723@gmail.com>
2022-08-10 19:48:04 +0100

    reset text value when icons are clicked

--- client/static/js/icons.js
+++ client/static/js/icons.js
@@ -10,29 +10,35 @@ for (let i = 0; i < allTextAreas.length; i++) {
 }
 
 camera.addEventListener("click", () => {
+  yourStory.value = "";
   yourStory.placeholder = "Place your photo(s) here";
 });
 
 left.addEventListener("click", () => {
+  yourStory.value = "";
   yourStory.placeholder =
     "Paste a YouTube, Vimeo or Twitter link, and press Enter";
 });
 
 right.addEventListener("click", () => {
+  yourStory.value = "";
   yourStory.placeholder =
     "Paste a YouTube, Vimeo or Twitter link, and press Enter";
 });
 
 camera.addEventListener("click", () => {
+  yourStory2.value = "";
   yourStory2.placeholder = "Place your photo(s) here";
 });
 
 left.addEventListener("click", () => {
+  yourStory2.value = "";
   yourStory2.placeholder =
     "Paste a YouTube, Vimeo or Twitter link, and press Enter";
 });
 
 right.addEventListener("click", () => {
+  yourStory2.value = "";
   yourStory2.placeholder =
     "Paste a YouTube, Vimeo or Twitter link, and press Enter";
 });
