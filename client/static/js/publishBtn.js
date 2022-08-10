300ce0d4c42c30e5458681ccf300130c3b9ec4be
vivian <vivian.v18723@gmail.com>
2022-08-10 19:48:42 +0100

    increase input allowance for textarea

--- client/static/js/publishBtn.js
+++ client/static/js/publishBtn.js
@@ -13,15 +13,15 @@ for (let i = 0; i < bothBtns.length; i++) {
     // displays red message for smaller screens
     if (getWidth() < 992) {
       // input handling
-      if (title.value.length > 40) {
+      if (title.value.length > 2000) {
         warningPara.textContent = "Title is too big";
       } else if (title.value.length === 0) {
         warningPara.textContent = "Title cannot be empty";
-      } else if (yourName.value.length > 40) {
+      } else if (yourName.value.length > 2000) {
         warningPara.textContent = "Author is too big";
       } else if (yourName.value.length === 0) {
         warningPara.textContent = "Author cannot be empty";
-      } else if (yourStory.value.length > 40) {
+      } else if (yourStory.value.length > 2000) {
         warningPara.textContent = "Your story is too big";
       } else if (yourStory.value.length === 0) {
         warningPara.textContent = "Your story cannot be empty";
@@ -34,15 +34,15 @@ for (let i = 0; i < bothBtns.length; i++) {
     // displays red message for larger screens
     if (getWidth() >= 992) {
       // input handling
-      if (title.value.length > 40) {
+      if (title.value.length > 2000) {
         warningPara.textContent = "Title is too big";
       } else if (title.value.length === 0) {
         warningPara.textContent = "Title cannot be empty";
-      } else if (yourName.value.length > 40) {
+      } else if (yourName.value.length > 2000) {
         warningPara.textContent = "Author is too big";
       } else if (yourName.value.length === 0) {
         warningPara.textContent = "Author cannot be empty";
-      } else if (yourStory2.value.length > 40) {
+      } else if (yourStory2.value.length > 2000) {
         warningPara.textContent = "Your story is too big";
       } else if (yourStory2.value.length === 0) {
         warningPara.textContent = "Your story cannot be empty";
